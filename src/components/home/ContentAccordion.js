f57a1684b6aedec56ceb64a9ee11cd75e1679f05
LorenzoZaccagnini <lorenzozcg@gmail.com>
2020-01-03 20:01:57 +0100

    fix obj

--- src/components/home/ContentAccordion.js
+++ src/components/home/ContentAccordion.js
@@ -3,56 +3,72 @@ import {h, render, Component} from 'preact';
 
 let strings = new LocalizedStrings({
  en:{
-   title: [
-     'I want to try Devoleum!',
-     'How Devoleum integrates Smart Tags?',
-     'What tech does Devoleum use?',
-     'Deveolum is a software or hardware solution?',
-     'Do you have a white paper?',
-     'Do you plan an ICO?'
-   ],
-   faq_content: [
-     "You can try our simulation for free, build your supply chain at this link.",
-     "Take a look at the video in the link, using different kinds of Oracles Devoleum can integrate even more advanced smart tags.",
-     "All the tech behind Devoleum is open source, Ethereum blockchain, Tensor Flow, PySyft, Truffle, React, Preact, Electron etc...",
-     "Devoleum is a digital solution, we manage the data provided by the users, we do not generate data or provide hardware tools.",
-     "No, we are busy working on the real project and is in constant evolution, we do not have time to waste on a fake paper, we suggest to read this article",
-     "No, we do not beg for money, if you want to invest in Devoleum you are welcome, contact us and do not forget to link your investments portfolio."
-   ],
-   link: [
-     'Link to simulation',
-     'Smart tag video',
-     '',
-     '',
-     'Article link',
-     'Contact us'
+   faq: [
+     {
+       title: 'I want to try Devoleum!',
+       content: "You can try our simulation for free, build your supply chain at this link.",
+       link: 'Smart tag video'
+     },
+     {
+       title: 'How Devoleum integrates Smart Tags?',
+       content: "Take a look at the video in the link, using different kinds of Oracles Devoleum can integrate even more advanced smart tags.",
+       link: 'Link to simulation'
+     },
+     {
+       title: 'What tech does Devoleum use?',
+       content: "All the tech behind Devoleum is open source, Ethereum blockchain, Tensor Flow, PySyft, Truffle, React, Preact, Electron etc...",
+       link: ''
+     },
+     {
+       title: 'Deveolum is a software or hardware solution?',
+       content: "Devoleum is a digital solution, we manage the data provided by the users, we do not generate data or provide hardware tools.",
+       link: ''
+     },
+     {
+       title: 'Do you have a white paper?',
+       content: "No, we are busy working on the real project and is in constant evolution, we do not have time to waste on a fake paper, we suggest to read this article",
+       link: 'Article link'
+     },
+     {
+       title: 'Do you plan an ICO?',
+       content: "No, we do not beg for money, if you want to invest in Devoleum you are welcome, contact us and do not forget to link your investments portfolio.",
+       link: 'Contact us'
+     },
    ]
  },
  it: {
-   title: [
-     'Voglio provare Devoleum!',
-     "In che modo Devoleum integra gli smart tag?",
-     "Quale tecnologie utilizza Devoleum?",
-     "Deveolum è una soluzione software o hardware?",
-     'Avete un whitepaper?',
-     "Pianificate una ICO?"
+   faq: [
+     {
+       title: 'Voglio provare Devoleum!',
+       content: "Puoi provare la nostra simulazione gratuitamente, simulare la costruzione della tua filiera a questo link.",
+       link: 'Link alla simulazione'
+     },
+     {
+       title: 'In che modo Devoleum integra gli smart tag?',
+       content: "Guarda il video nel link, utilizzando diversi tipi di oracoli Devoleum può integrare smart tag ancora più avanzati.",
+       link: 'Smart tag video'
+     },
+     {
+       title: 'Quale tecnologie utilizza Devoleum?',
+       content: "Tutta la tecnologia alla base di Devoleum è open source, blockchain di Ethereum, Tensor Flow, PySyft, Truffle, React, Preact, Electron ecc ...",
+       link: ''
+     },
+     {
+       title: 'Deveolum è una soluzione software o hardware?',
+       content: "Devoleum è una soluzione digitale, i dati sono forniti dagli utenti, non generiamo dati o forniamo strumenti hardware.",
+       link: ''
+     },
+     {
+       title: 'Avete un whitepaper?',
+       content: "No, siamo impegnati a lavorare sul progetto concreto ed è in continua evoluzione, non abbiamo tempo da perdere su un documento fatto di promesse, ti consigliamo di leggere questo articolo",
+       link: "Collegamento dell'articolo"
+     },
+     {
+       title: 'Pianificate una ICO?',
+       content: "No, non chiediamo soldi, se vuoi investire in Devoleum sei il benvenuto, contattaci e non dimenticare di collegare il tuo portfolio di investimenti.",
+       link: 'Contattaci'
+     },
    ],
-   faq_content: [
-        "Puoi provare la nostra simulazione gratuitamente, simulare la costruzione della tua filiera a questo link.",
-        "Guarda il video nel link, utilizzando diversi tipi di oracoli Devoleum può integrare smart tag ancora più avanzati.",
-        "Tutta la tecnologia alla base di Devoleum è open source, blockchain di Ethereum, Tensor Flow, PySyft, Truffle, React, Preact, Electron ecc ...",
-        "Devoleum è una soluzione digitale, i dati sono forniti dagli utenti, non generiamo dati o forniamo strumenti hardware.",
-        "No, siamo impegnati a lavorare sul progetto concreto ed è in continua evoluzione, non abbiamo tempo da perdere su un documento fatto di promesse, ti consigliamo di leggere questo articolo",
-        "No, non chiediamo soldi, se vuoi investire in Devoleum sei il benvenuto, contattaci e non dimenticare di collegare il tuo portfolio di investimenti."
-      ],
-   link: [
-     "Link alla simulazione",
-     "Smart tag video",
-     '',
-     '',
-     "Collegamento dell'articolo",
-     'Contattaci'
-   ]
  }
 });
 
@@ -77,19 +93,19 @@ export default class ContentAccordion extends Component {
           <h2 className="title">FAQ</h2>
 
           <div>
-              {strings.title.map((item, index) => {
+              {strings.faq.map((item, index) => {
               return <div className="accordion faq">
                   <div className="divider"></div>
                   <input type="checkbox" id={ "accordion-" + index} name="accordion-checkbox" hidden />
                   <label className="accordion-header title" for={ "accordion-" + index}>
-                      <i className="icon icon-arrow-right mr-1"></i> {item}
+                      <i className="icon icon-arrow-right mr-1"></i> {item.title}
                   </label>
                   <div className="accordion-body sub-content faq-text">
-                      {strings.faq_content[index]}
+                      {item.content}
                       <br />
                       <br />
                       <a className="accordion-body sub-content" href={LINKS[index]}>
-                                {strings.link[index]}
+                                {item.link}
                               </a>
                   </div>
 
