33881aeea3c9dcb7344bbc734f1b5f060cb19f54
lorenzozaccagnini <lorenzozcg@gmail.com>
2021-06-22 00:35:48 +0200

    Update DevoleumIntro.js

--- src/components/home/DevoleumIntro.js
+++ src/components/home/DevoleumIntro.js
@@ -4,18 +4,18 @@ import LocalizedStrings from 'localized-strings';
 let strings = new LocalizedStrings({
 	en: {
 		title: 'What is Devoleum?',
-		content: 'Devoleum is a web app. Devoleum organizes data from physical or digital supply chains into meaningful stories, making each step immutable on blockchain or other distributed systems. Devoleum automatically builds a storyboard of the product, easily accessible on our web app from any device just by scanning a smart tag. Devoleum is an open source project, free and open to everyone.',
+		content: 'Devoleum is a web app that transforms the linked open data of physical or digital supply chains into meaningful stories, making each step immutable on blockchain or other distributed systems. Each story shows in a clear and detailed way the steps that contributed to making the product unique and precious. Devoleum automatically creates a product storytelling, easily accessible on our platform from any device simply by scanning a QR code. Devoleum is an open source project, free and open to everyone.',
 		calltoaction: 'Explore'
 	},
 	it: {
 		title: 'Cosa è Devoleum?',
-		content: 'Devoleum è una web app. Devoleum organizza i dati di filiere fisiche o digitali in storie significative, rendendo ogni passaggio immutabile su blockchain o altri sistemi distribuiti. Ogni storia mostra in modo chiaro e dettagliato i passaggi che hanno contribuito a rendere il prodotto unico e prezioso. Devoleum crea automaticamente uno storytelling del prodotto, facilmente accessibile sulla nostra piattaforma da qualsiasi dispositivo semplicemente scansionando uno smart tag. Devoleum è un progetto open source, gratuito e aperto a tutti.',
-		calltoaction: 'Esplora le Storie'
+		content: 'Devoleum è una web app che trasforma i linked open data di filiere fisiche o digitali in storie significative, rendendo ogni passaggio immutabile su blockchain o altri sistemi distribuiti. Ogni storia mostra in modo chiaro e dettagliato i passaggi che hanno contribuito a rendere il prodotto unico e prezioso. Devoleum crea automaticamente uno storytelling del prodotto, facilmente accessibile sulla nostra piattaforma da qualsiasi dispositivo semplicemente scansionando un QR code. Devoleum è un progetto open source, gratuito e aperto a tutti.',
+		calltoaction: 'Esplora'
 	},
 	fr: {
 		title: "Qu'est-ce que Devoleum?",
-		content: "Devoleum est une application Web. Devoleum organise les données des chaînes d'approvisionnement physiques ou numériques en histoires significatives, rendant chaque étape immuable sur la blockchain ou d'autres systèmes distribués. Chaque histoire montre clairement et en détail les étapes qui ont contribué à rendre le produit unique et précieux. Devoleum crée automatiquement un historique du produit, facilement accessible sur notre plateforme à partir d'un appareil, simplement en scannant une smart tag. Devoleum est un projet open source, gratuit et ouvert à tous.",
-		calltoaction: 'Visitez notre plateforme'
+		content: "Devoleum est une application Web qui transforme les données ouvertes liées des chaînes d'approvisionnement physiques ou numériques en histoires significatives, rendant chaque étape immuable sur la blockchain ou d'autres systèmes distribués. Chaque histoire montre de manière claire et détaillée les étapes qui ont contribué à rendre le produit unique et précieux. Devoleum crée automatiquement un storytelling de produit, facilement accessible sur notre plateforme depuis n'importe quel appareil en scannant simplement un code QR. Devoleum est un projet open source, gratuit et ouvert à tous.",
+		calltoaction: 'Explorer'
 	}
 });
 
